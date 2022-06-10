89284ef1010e8ced4ff0618644d5b7e4e38595cf
Josh Porter <joshporterdev@protonmail.com>
2022-06-09 19:48:17 -0500

    added telescope

--- lua/mappings/init.lua
+++ lua/mappings/init.lua
@@ -2,3 +2,5 @@ local map = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
 vim.g.mapleader = ' '
+
+map('n', '<S-f>', ':Telescope find_files<CR>', opts)
--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -1,6 +1,9 @@
 return require('packer').startup(function()
   use 'wbthomason/packer.nvim'
   
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+  
   -- Color Schemes
   use 'lucasprag/simpleblack'
 end)
