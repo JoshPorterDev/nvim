0f2102940bff7fdf6027bac841afe478f2293d02
Josh Porter <joshporterdev@protonmail.com>
2022-06-10 18:36:58 -0500

    added cmp

--- lua/packer-config/init.lua
+++ lua/packer-config/init.lua
@@ -7,6 +7,17 @@ return require('packer').startup(function()
   use 'nvim-treesitter/nvim-treesitter'
   use 'neovim/nvim-lspconfig'
   use 'williamboman/nvim-lsp-installer'
+  
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/nvim-cmp'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'onsails/lspkind.nvim'
+  
+  use {"windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end}
 
   -- Color Schemes
   use 'lucasprag/simpleblack'
