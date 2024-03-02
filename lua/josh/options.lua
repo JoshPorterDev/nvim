b505a916cc9de2ce50bac9f0d39c14a23e413cc0
Josh Porter <josh@joshporter.xyz>
2024-03-02 15:34:34 -0600

    Add basic options

---
+++ lua/josh/options.lua
@@ -0,0 +1,10 @@
+local set = vim.opt
+
+set.number = true
+set.numberwidth = 4
+set.expandtab = true
+set.shiftwidth = 4
+set.tabstop = 4
+set.autoindent = true
+set.smartindent = true
+set.scrolloff = 10
