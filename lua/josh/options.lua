ad7fb79ab4bccaec86706b83189a73d1dbce4e44
Josh Porter <josh@joshporter.xyz>
2024-03-02 22:48:43 -0600

    Add netrw options

--- lua/josh/options.lua
+++ lua/josh/options.lua
@@ -9,3 +9,9 @@ set.autoindent = true
 set.smartindent = true
 set.scrolloff = 10
 set.wrap = false
+
+-- Netrw
+vim.g.netrw_banner = 0
+vim.g.netrw_liststyle = 3
+vim.g.netrw_browse_split = 4
+vim.g.netrw_winsize = 25
